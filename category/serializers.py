d55495d9d620e07be7b28ff390094c0612ff9788
shakti505 <shakti@gkmit.co>
2025-02-05 19:02:07 +0530

    fix: category updated

--- category/serializers.py
+++ category/serializers.py
@@ -9,13 +9,15 @@ class CategorySerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Category
-        fields = ["id", "name", "user", "is_predefined","type"]
-        read_only_fields = ["id", "is_predefined", ]
+        fields = ["id", "name", "user", "is_predefined", "type"]
+        read_only_fields = [
+            "id",
+            "is_predefined",
+        ]
 
     def __init__(self, *args, **kwargs):
         # Get the request object from the context
         request = kwargs.get("context", {}).get("request")
-
 
         # If there's no request, proceed as usual
         if request:
@@ -28,26 +30,31 @@ class CategorySerializer(serializers.ModelSerializer):
         super().__init__(*args, **kwargs)
 
     def validate_user(self, user):
-        """Custom validation for user: Ensure the user is active, 
-        normal users can only create categories for themselves, 
+        """Custom validation for user: Ensure the user is active,
+        normal users can only create categories for themselves,
         and staff cannot create categories for other staff."""
         request = self.context.get("request")
 
         # Ensure the user exists and is active
         if not user or not user.is_active:
-            raise serializers.ValidationError("The specified user is either invalid or inactive.")
+            raise serializers.ValidationError(
+                "The specified user is either invalid or inactive."
+            )
 
         # Normal users can only create categories for themselves
         if not request.user.is_staff and user != request.user:
-            raise PermissionDenied("Normal users can only create categories for themselves.")
+            raise PermissionDenied(
+                "Normal users can only create categories for themselves."
+            )
 
         # Staff users can create categories for themselves, but not for other staff users
-        if request.user.is_staff and user != request.user:
-            raise PermissionDenied("Staff users cannot create categories for other staff users.")
+        if request.user.is_staff:
+            if user != request.user and user.is_staff:
+                raise PermissionDenied(
+                    "Staff users cannot create categories for other staff users."
+                )
 
         return user
-
-
 
     def _validate_name(self, value, user, type):
         """Validate that the category name is unique for the determined user."""
@@ -69,7 +76,6 @@ class CategorySerializer(serializers.ModelSerializer):
                 }
             )
 
-
         return value
 
     def validate(self, data):
@@ -80,7 +86,7 @@ class CategorySerializer(serializers.ModelSerializer):
             # If the user is not staff, force the user field to be the authenticated user
             user = data.get("user", request.user)
             if "name" in data:
-                data["name"] = self._validate_name(data["name"], user, data.get('type'))
+                data["name"] = self._validate_name(data["name"], user, data.get("type"))
 
             data["user"] = user
             data["is_predefined"] = user.is_staff
@@ -89,7 +95,9 @@ class CategorySerializer(serializers.ModelSerializer):
         else:
             # Prevent staff users from updating the user field
             if "name" in data:
-                data["name"] = self._validate_name(data["name"], self.instance.user, self.instance.type)
+                data["name"] = self._validate_name(
+                    data["name"], self.instance.user, self.instance.type
+                )
 
         return data
 
